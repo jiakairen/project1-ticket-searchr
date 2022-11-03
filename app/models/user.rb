aa6f63780c93e7b21dd6f22ac3a7e9005f045010
Jiakai Ren <jiakai.ren@gmail.com>
2022-11-03 13:37:16 +1100

    improved bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, :uniqueness => true, :presence => true
     has_many :tickets
+
+    def money_spent
+        spent = 0
+        self.tickets.each do |ticket|
+            spent += ticket.price
+        end
+        spent
+    end
 end
